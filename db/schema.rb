736248f906bb2a5055a3fa33f5252e182ba4f7a1
Adam Gamble <adamgamble@gmail.com>
2013-08-17 22:28:30 -0500

    added actor and movie_role

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130817210330) do
+ActiveRecord::Schema.define(version: 20130818032711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "movie_roles", force: true do |t|
+    t.integer  "movie_id"
+    t.integer  "actor_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "movie_studios", force: true do |t|
     t.string   "name"
